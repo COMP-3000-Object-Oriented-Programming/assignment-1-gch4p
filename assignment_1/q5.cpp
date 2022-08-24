d630faabfd1be7795b8507f897e20d02d19ab7cd
gch4p <gpc0008@auburn.edu>
2022-08-24 09:48:45 -0500

    added q5 solution

--- assignment_1/q5.cpp
+++ assignment_1/q5.cpp
@@ -2,7 +2,19 @@
 
 using namespace std; 
 
-int main (){
+int main() {
+	double guess = 0;
+	int input = 0;
+	double n = 0;
+	double r = 0;
 
-    
+	std::cin >> input;
+	n = static_cast<double>(input);
+	guess = n / 2;
+	for (int i = 0; i < 5; ++i) {
+		r = n / guess;
+		guess = (guess + r) / 2;
+	}
+	std::cout << guess;
+	system("pause");
 }
